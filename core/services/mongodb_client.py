e0ed0b42253821b507098658a882d519e9d381fb
drupman <tomasgaudino@hotmail.com>
2025-01-03 14:37:33 -0300

    (debug) add temp print for docker debugging

--- core/services/mongodb_client.py
+++ core/services/mongodb_client.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from datetime import datetime, timedelta
+import logging
 
 
 class MongoDBClient:
@@ -31,7 +32,7 @@ class MongoDBClient:
     async def connect(self):
         """Connect to MongoDB using provided or environment variables."""
         connection_string = f"mongodb://{self.username}:{self.password}@{self.host}:{self.port}/?authSource=admin"
-        
+        logging.info(connection_string)
         try:
             self.client = AsyncIOMotorClient(
                 connection_string,
