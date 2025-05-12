fa790e01cf1fbaac7901f8737b8bb427e201331f
drupman <tomasgaudino@hotmail.com>
2025-05-12 11:34:01 -0300

    (feat) add class DashBackend

--- core/performance/dash/backend.py
+++ core/performance/dash/backend.py
@@ -1,41 +1,47 @@
-# backend.py
-import os
-import json
-import asyncio
+from typing import Dict
+
 import pandas as pd
 from dotenv import load_dotenv
+
+from core.performance.sync_manager import DatabaseSyncManager, ServerHandler
 from core.services.backend_api_client import BackendAPIClient
 
 load_dotenv()
 
 
-async def fetch_servers_info():
-    backend_api_servers_str = os.getenv("BACKEND_API_SERVERS", '{"master": "localhost"}')
-    backend_api_servers = json.loads(backend_api_servers_str)
+class DashBackend:
+    def __init__(self, sync_manager: DatabaseSyncManager):
+        self.sync_manager = sync_manager
 
-    backend_api_info = []
-    for backend_api_name, backend_api_host in backend_api_servers.items():
-        try:
-            backend_api_client = BackendAPIClient(backend_api_host)
-            archived_dbs = await backend_api_client.list_databases()
-            running_bots = await backend_api_client.get_active_bots_status()
-            info_dict = {
-                "name": backend_api_name,
-                "host": backend_api_host,
-                "archived_databases": len(archived_dbs),
-                "running_bots": len(running_bots["data"]),
-                "status": "Connected"
-            }
-        except Exception as e:
-            info_dict = {
-                "name": backend_api_name,
-                "host": backend_api_host,
-                "archived_databases": 0,
-                "running_bots": 0,
-                "status": "Disconnected"
-            }
-        backend_api_info.append(info_dict)
+    async def fetch_servers_info(self):
+        servers: Dict[str, ServerHandler] = self.sync_manager.servers
+        servers_info = []
+        for name, server in servers.items():
+            try:
+                backend_api_client = BackendAPIClient(server.host)
+                running_bots = await backend_api_client.get_active_bots_status()
+                local_dbs = server.local_dbs
+                server_dbs = server.remote_db_names
+                missing_dbs = server.missing_dbs_names
+                info_dict = {
+                    "name": name,
+                    "host": server.host,
+                    "local_databases": len(local_dbs),
+                    "server_dbs": len(server_dbs),
+                    "missing_dbs": len(missing_dbs),
+                    "running_bots": len(running_bots["data"]),
+                    "status": "Connected"
+                }
+            except Exception as e:
+                info_dict = {
+                    "name": name,
+                    "host": server.host,
+                    "local_databases": 0,
+                    "server_dbs": 0,
+                    "missing_dbs": 0,
+                    "running_bots": 0,
+                    "status": "Disconnected"
+                }
+            servers_info.append(info_dict)
 
-    return pd.DataFrame(backend_api_info)
-
-
+        return pd.DataFrame(servers_info)
